47dc739c701cc06e20fcb527d40de311e51076a1
haohao809 <haohao809@126.com>
2019-12-18 15:40:36 +0800

    commit first

---
+++ main.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    const canvas = document.getElementById("canvas"),
+    context = canvas.getContext("2d"),
+    width = canvas.width = window.innerWidth,
+    height = canvas.height = window.innerHeight;          
+}
