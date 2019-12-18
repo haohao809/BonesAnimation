897c38ead46d6fe45e263224088da7b05ea7ca4b
haohao809 <haohao809@126.com>
2019-12-18 21:21:24 +0800

    显示基本骨骼动画

--- main.js
+++ main.js
@@ -4,5 +4,18 @@ window.onload = function() {
     width = canvas.width = window.innerWidth,
     height = canvas.height = window.innerHeight;
     const arm = Arm.create(width/2, height/2,100, 0.1);
+    const arm2 = Arm.create(arm.getEndX(),arm.getEndY(),100,0);
+    angle = 0;
+    update();
+    function update() {
+        context.clearRect(0,0,width,height);
+        arm.angle = Math.sin(angle) *1.2;
+        arm2.x = arm.getEndX();
+        arm2.y = arm.getEndY();
+        angle += 0.05;
+        arm.render(context);
+        arm2.render(context);
+        requestAnimationFrame(update)
+    }
     arm.render(context);          
 }
